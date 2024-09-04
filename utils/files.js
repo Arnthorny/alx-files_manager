1b43504e52442156abd1167197fddd8d4fde190a
Arnthorny <chidubem.anthony26@gmail.com>
2024-09-04 10:50:11 -0700

    fix: Added ternary operator check to see if id is valid

--- utils/files.js
+++ utils/files.js
@@ -1,19 +1,25 @@
-import { v4 as uuidv4 } from 'uuid';
-import { promises as fs } from 'fs';
-import path from 'path';
-import { ObjectID } from 'mongodb';
-import dbClient from './db';
+import { v4 as uuidv4 } from "uuid";
+import { promises as fs } from "fs";
+import path from "path";
+// import { ObjectID } from "mongodb";
+// import ObjectID from "mongodb";
+
+import BSON from "bson";
+const ObjectID = BSON.ObjectID;
+
+import dbClient from "./db.js";
 
 // const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-const fileExists = (path) => fs.stat(path).then(
-  () => true,
-  () => false,
-);
+const fileExists = (path) =>
+  fs.stat(path).then(
+    () => true,
+    () => false
+  );
 
 async function createLocalFile(b64Data) {
-  const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+  const folderPath = process.env.FOLDER_PATH || "/tmp/files_manager";
   try {
-    const dataBuffer = Buffer.from(b64Data, 'base64');
+    const dataBuffer = Buffer.from(b64Data, "base64");
     if (!(await fileExists(folderPath))) {
       await fs.mkdir(folderPath);
     }
@@ -38,15 +44,18 @@ async function publishHelper(req, res, bool) {
   const userId = req.user._id.toString();
   const fileId = req.params.id;
 
+  const usr_obj_id = ObjectID.isValid(userId) ? ObjectID(userId) : undefined;
+  const file_obj_id = ObjectID.isValid(fileId) ? ObjectID(fileId) : undefined;
+
   const updateResp = await (
     await dbClient.filesCollection()
   ).findOneAndUpdate(
-    { _id: ObjectID(fileId), userId: ObjectID(userId) },
+    { _id: file_obj_id, userId: usr_obj_id },
     { $set: { isPublic: bool } },
-    { returnDocument: 'after' },
+    { returnDocument: "after" }
   );
   if (!updateResp.value) {
-    res.status(404).json({ error: 'Not found' });
+    res.status(404).json({ error: "Not found" });
   } else {
     updateResp.value.id = String(updateResp.value._id);
     delete updateResp.value._id;
@@ -56,6 +65,4 @@ async function publishHelper(req, res, bool) {
   }
 }
 
-export {
-  createLocalFile, fileExists, publishHelper, readLocalFile,
-};
+export { createLocalFile, fileExists, publishHelper, readLocalFile };
